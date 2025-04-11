dff2c1ac6444214ba4805d3baecb2e36e8b1e67c
00HelloKing <jkf1018407510@163.com>
2025-04-11 09:35:51 +0800

    Added the function of getting names

--- reaction.py
+++ reaction.py
@@ -11,7 +11,8 @@ sleep(uniform(5, 10))
 led.off()
 
 def pressed(button):
-    print(str(button.pin.number) + ' won the game')
-
-right_button.when_pressed = pressed
-left_button.when_pressed = pressed
+    if button.pin.number == 14:
+        print(left_name + ' won the game')
+    else:
+        print(right_name + ' won the game')
+    exit()
